157427f66235bcabf22857533002d2eb12caec52
Luay Younus <luayghazwan@yahoo.com>
2017-10-23 15:19:31 -0700

    Flight Controller and Search Model created to Route Searches and Sorting

---
+++ AlaskaAirlines/AlaskaAirlines/ViewModels/SearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AlaskaAirlines.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlaskaAirlines.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string FromAirport { get; set; }
+        public string ToAirport { get; set; }
+        public List<Flight> Flights { get; set; }
+    }
+}
