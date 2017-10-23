93fc135d61244cb8002ba8ee7eb6e0c4e49c1e53
Luay Younus <luayghazwan@yahoo.com>
2017-10-23 14:56:46 -0700

    Airport and Flight Models created

---
+++ AlaskaAirlines/AlaskaAirlines/Models/Airport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlaskaAirlines.Models
+{
+    public class Airport
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
