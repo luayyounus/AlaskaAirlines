41b2fd531f628cbbdc192a6d2a0dbba3ecb4ede0
Luay Younus <luayghazwan@yahoo.com>
2017-11-07 22:59:36 -0800

    Home Controller testing

--- AlaskaAirlines/AlaskaAirlines/Controllers/HomeController.cs
+++ AlaskaAirlines/AlaskaAirlines/Controllers/HomeController.cs
@@ -13,14 +13,13 @@ namespace AlaskaAirlines.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            List<Flight> flights = new List<Flight>();
-            var viewModel = new SearchViewModel
+            var emptySearchView = new SearchViewModel
             {
                 FromAirport = "",
                 ToAirport = "",
-                Flights = flights
+                Flights = new List<Flight>()
             };
-            return View(viewModel);
+            return View(emptySearchView);
         }
 
         [HttpPost]
@@ -33,10 +32,10 @@ namespace AlaskaAirlines.Controllers
             return Json(Airport, JsonRequestBehavior.AllowGet);
         }
 
+
         [HttpPost]
         public ActionResult Search(SearchViewModel searchViewModel)
         {
-
             //Form Validation
             if (!ModelState.IsValid)
             {
@@ -50,43 +49,82 @@ namespace AlaskaAirlines.Controllers
                 return View("Index", sameViewModel);
             }
 
-            List<Airport> listOfAirports = Csv.GetAllAirports();
+            var fromCode = "SEA";
+            var toCode = "LAX";
 
-            //Matching the airport's code with CSV
-            var fromCode = "";
-            var toCode = "";
-
-            foreach (Airport airport in listOfAirports)
-            {
-                if (airport.Name == searchViewModel.FromAirport || airport.Code == searchViewModel.FromAirport)
-                {
-                    fromCode = airport.Code;
-                }
-                if (airport.Name == searchViewModel.ToAirport || airport.Code == searchViewModel.ToAirport)
-                {
-                    toCode = airport.Code;
-                }
-            }
-
-            return RedirectToAction("Availability", "Home", new { fromAirport = fromCode, toAirport = toCode });
-        }
-
-
-        [Route("home/availability/{fromAirport:maxlength(3):minlength(3)}/{toAirport:maxlength(3):minlength(3)}/")]
-        [HttpGet]
-        public ActionResult Availability(string fromAirport, string toAirport)
-        {
-
-            List<Flight> flights = Csv.SearchFlights(fromAirport, toAirport);
+            List<Flight> flights = Csv.SearchFlights(fromCode, toCode);
 
             var viewModel = new SearchViewModel
             {
-                FromAirport = fromAirport,
-                ToAirport = toAirport,
+                FromAirport = searchViewModel.FromAirport,
+                ToAirport = searchViewModel.ToAirport,
                 Flights = flights
             };
-
-            return View("index", viewModel);
+            return View("Index", viewModel);
         }
+
+        //List<Airport> listOfAirports = Csv.GetAllAirports();
+
+        //foreach (Airport airport in listOfAirports)
+        //{
+        //    if (airport.Name == searchViewModel.FromAirport || airport.Code == searchViewModel.FromAirport)
+        //    {
+        //        fromCode = airport.Code;
+        //    }
+        //    if (airport.Name == searchViewModel.ToAirport || airport.Code == searchViewModel.ToAirport)
+        //    {
+        //        toCode = airport.Code;
+        //    }
+        //}
+
+        //return RedirectToAction("Availability", "Home", new { fromAirport = fromCode, toAirport = toCode });
+
+
+        //[Route("home/availability/{fromAirport:maxlength(3):minlength(3)}/{toAirport:maxlength(3):minlength(3)}/{sortBy?}")]
+        //[HttpGet]
+        //public ActionResult Availability(string fromAirport, string toAirport, string sortBy)
+        //{
+
+        //    ViewBag.SortByFlight = string.IsNullOrWhiteSpace(sortBy) ? "Flight" : "";
+        //    ViewBag.SortByDeparture = "Departure";
+        //    ViewBag.SortByPrice = "Price";
+
+        //    List<Flight> sortedFlights = new List<Flight>();
+
+        //    if (!String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        if (sortBy == "Flight")
+        //        {
+        //            sortedFlights = flights.OrderBy(flight => flight.FlightNumber).ToList();
+        //        }
+        //        else if (sortBy == "Departure")
+        //        {
+        //            sortedFlights = flights.OrderBy(flightTime => flightTime.Departs.TimeOfDay).ToList();
+        //        }
+        //        else if (sortBy == "Price")
+        //        {
+        //            sortedFlights = flights.OrderBy(flightPrice => flightPrice.MainCabinPrice).ToList();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        sortedFlights = flights;
+        //    }
+
+        //    var viewModel = new SearchViewModel
+        //    {
+        //        FromAirport = fromAirport,
+        //        ToAirport = toAirport,
+        //        Flights = sortedFlights
+        //    };
+        //    return View("index", viewModel);
+        //}
+
+        //[HttpGet]
+        //public ActionResult SortFlights(SearchViewModel searchModel, string sortBy)
+        //{
+        //    var viewModel = Availability(searchModel.FromAirport, searchModel.ToAirport, sortBy);
+        //    return PartialView("_Flights", viewModel);
+        //}
     }
 }
