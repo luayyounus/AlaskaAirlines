3a423dbfdc610d452a867f63bfd09581448314fe
Luay Younus <luayghazwan@yahoo.com>
2017-11-09 00:12:16 -0800

    seperated js files from cshtml view

---
+++ AlaskaAirlines/AlaskaAirlines/Scripts/jquery-autocomplete.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $("#FromAirport, #ToAirport").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/Home/AutoComplete',
+                type: 'POST',
+                dataType: 'json',
+                data: { Prefix: request.term },
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return { label: item.Name, value: item.Name };
+                    }));
+                }
+            });
+        },
+        change: function (event, ui) {
+            if (!ui.item) {
+                $(this).val('');
+                $this.focus();
+                $();
+            }
+        },
+        autoFocus: true
+    });
+});
---
+++ AlaskaAirlines/AlaskaAirlines/Scripts/jquery-form-submission.js
@@ -0,0 +1,16 @@
+﻿function submitForm() {
+    $("#submit-button").click(function () {
+        var from = $("#FromAirport").val();
+        var to = $("#ToAirport").val();
+        $.ajax({
+            url: '/Home/Search',
+            method: 'POST',
+            cache: false,
+            data: { FromAirport: from, ToAirport: to }
+        }).done(function (response) {
+            $("#flights-results").html(response);
+        }).fail(function () {
+            alert("failed submitting form");
+        });
+    });
+}
