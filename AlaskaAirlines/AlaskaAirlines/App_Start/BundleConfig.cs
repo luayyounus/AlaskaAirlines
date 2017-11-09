4c03cbc9befa97d38849dda8878f906bf87066c6
Luay Younus <luayghazwan@yahoo.com>
2017-11-08 20:36:56 -0800

    updated bundle config to include new scripts

--- AlaskaAirlines/AlaskaAirlines/App_Start/BundleConfig.cs
+++ AlaskaAirlines/AlaskaAirlines/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace AlaskaAirlines.App_Start
                        "~/Scripts/jquery-{version}.js",
                        "~/Scripts/jquery-ui-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                       "~/Scripts/jquery.validate*"));
+                       "~/Scripts/jquery.validate*",
+                       "~/Scripts/jquery.validate.unobtrusive.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
