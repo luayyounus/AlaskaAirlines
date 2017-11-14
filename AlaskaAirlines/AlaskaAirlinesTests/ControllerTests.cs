64e7b5523a9cba71213df35f3ffa25e9d29c45b9
Luay Younus <luayghazwan@yahoo.com>
2017-11-14 01:38:01 -0800

    Form Submission test

--- AlaskaAirlines/AlaskaAirlinesTests/ControllerTests.cs
+++ AlaskaAirlines/AlaskaAirlinesTests/ControllerTests.cs
@@ -6,23 +6,51 @@ using System.Text;
 using System.Web.Mvc;
 using AlaskaAirlines;
 using AlaskaAirlines.Controllers;
+using AlaskaAirlines.ViewModels;
+using AlaskaAirlines.Models;
 
 namespace AlaskaAirlinesTests
 {
+
     [TestClass]
-    public class WhenRequestingTheIndexPage
+    public class WhenRequestingIndexPage
     {
         [TestMethod]
         public void ThenReturnTheHomeView()
         {
             //Arrange
             HomeController homeController = new HomeController();
-            
+
             //Act
             ViewResult viewResult = homeController.Index() as ViewResult;
-            
+
             //Assert
             Assert.IsNotNull(viewResult);
+        }
+    }
+   
+    [TestClass]
+    public class WhenSubmittingForm
+    {
+
+        [TestMethod]
+        public void ThenReturnSameViewIfEmpty()
+        {
+            //Arrange
+            SearchViewModel emptySearchViewModel = new SearchViewModel
+            {
+                FromAirport = "",
+                ToAirport = "",
+                Flights = null
+            };
+            
+            //Act
+            HomeController homeController = new HomeController();
+            homeController.ModelState.AddModelError("formIncomplete", "formIncomplete");
+            ViewResult result = homeController.Index(emptySearchViewModel) as ViewResult;
+
+            //Assert
+            Assert.IsTrue(result.ViewData.ModelState.Count > 0);
         }
     }
 }
